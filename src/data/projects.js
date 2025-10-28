f222820768b129dabcd3d2a2f8a0f2cc192e7099
Antonio Traversa <capa909@gmail.com>
2025-10-28 15:34:00 +0100

    Aggiornamento automatico file progetti

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,43 +1,30 @@
-import proj_travyplay_mainImg from '../assets/images/proj_travyplay/icon_travyplay.png';
-import feat_1761582326212_featImg0 from '../assets/images/proj_travyplay/Screenshot_TravyPlay6.png';
-import feat_1761582340878_featImg1 from '../assets/images/proj_travyplay/Screenshot_TravyPlay3.png';
-import feat_1761582367755_featImg2 from '../assets/images/proj_travyplay/Screenshot_TravyPlay9.png';
-import feat_1761582378195_featImg3 from '../assets/images/proj_travyplay/giflib.gif';
+
 
 export default [
+  {
+    "id": "proj_systemdashboard",
+    "title": "SystemDashboard",
+    "description": "Applicazione desktop per Windows che mostra una dashboard a schermo intero in tempo reale con le statistiche del sistema: utilizzo di CPU, RAM, GPU (solo NVIDIA), rete e attività del disco. È progettata per essere visualizzata su un monitor secondario, rimanendo sempre in primo piano.",
+    "image": null,
+    "tags": [
+      "Python",
+      "Windows"
+    ],
+    "features": [],
+    "repository": "https://github.com/antoniotraversa/SystemDashboard"
+  },
   {
     "id": "proj_travyplay",
     "title": "TravyPlay",
     "description": "Media center in Flutter per gestire musica, video, IPTV e libri.",
-    "image": proj_travyplay_mainImg,
+    "image": null,
     "tags": [
       "Dart",
       "Flutter",
       "Android",
       "IOS"
     ],
-    "features": [
-      {
-        "title": "Gestione Musica",
-        "description": "Organizza e riproduci la tua musica, Crea playlist o filtra per generi, artisti o album",
-        "imageUrl": feat_1761582326212_featImg0
-      },
-      {
-        "title": "Film & Serie TV",
-        "description": "Godi i tuoi contenuti video etichettabili e filtrabili per tipo, genere o raccolta",
-        "imageUrl": feat_1761582340878_featImg1
-      },
-      {
-        "title": "Liste IPTV",
-        "description": "Guarda canali da tutto il mondo in chiaro, filtrati per nazione o categoria",
-        "imageUrl": feat_1761582367755_featImg2
-      },
-      {
-        "title": "Libreria Intelligente",
-        "description": "Importa i tuoi eBook e documenti oppure noleggia gratuitamente da OpenLibrary & InternetArchive dalla la sezione dedicata.",
-        "imageUrl": feat_1761582378195_featImg3
-      }
-    ],
+    "features": [],
     "repository": "https://github.com/antoniotraversa/TravyPlay"
   }
 ];
