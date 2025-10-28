24f5a8591c25d8771240ab5fd177b35429d4b00c
Antonio <capa909@gmail.com>
2025-10-28 01:57:02 +0100

    test

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,46 +1,43 @@
-import travyplayImg from "../assets/images/travyplay/travyplaylogo.png";
-import travyplayImg2 from "../assets/images/travyplay/2.png";
-import travyplayImg3 from "../assets/images/travyplay/3.png";
-import travyplayImg4 from "../assets/images/travyplay/4.png";
-import travyplayImg10gif from "../assets/images/travyplay/10_gif.gif";
-
-
-
+import proj_travyplay_mainImg from '../assets/images/proj_travyplay/icon_travyplay.png';
+import feat_1761582326212_featImg0 from '../assets/images/proj_travyplay/Screenshot_TravyPlay6.png';
+import feat_1761582340878_featImg1 from '../assets/images/proj_travyplay/Screenshot_TravyPlay3.png';
+import feat_1761582367755_featImg2 from '../assets/images/proj_travyplay/Screenshot_TravyPlay9.png';
+import feat_1761582378195_featImg3 from '../assets/images/proj_travyplay/giflib.gif';
 
 export default [
   {
-    id: "Travyplay",
-    title: "TravyPlay",
-    description: "Media center in Flutter per gestire musica, video, IPTV e libri.",
-    image: travyplayImg,
-    tags: ["Dart", "Flutter", "IOS", "Android"],
-    repository: "https://github.com/antoniotraversa/TravyPlay",
-  features: [
-    {
-      title: "Gestione Musica",
-      description: "Organizza e riproduci la tua musica, Crea playlist o filtra per generi, artisti o album",
-      imageUrl: travyplayImg2
-    },
-
-    {
-      title: "Film & Serie TV",
-      description: "Godi i tuoi contenuti video etichettabili e filtrabili per tipo, genere o raccolta",
-      imageUrl: travyplayImg3 
-    },
-
-     {
-      title: "Liste IPTV",
-      description: "Guarda canali da tutto il mondo in chiaro, filtrati per nazione o categoria",
-      imageUrl: travyplayImg4
-    },
-    
-    {
-      title: "Libreria Intelligente",
-      description: "Importa i tuoi eBook e documenti oppure noleggia gratuitamente da OpenLibrary & InternetArchive dalla la sezione dedicata.",
-      imageUrl: travyplayImg10gif 
-    },
-    
-  ]
-  },
-
+    "id": "proj_travyplay",
+    "title": "TravyPlay",
+    "description": "Media center in Flutter per gestire musica, video, IPTV e libri.",
+    "image": proj_travyplay_mainImg,
+    "tags": [
+      "Dart",
+      "Flutter",
+      "Android",
+      "IOS"
+    ],
+    "features": [
+      {
+        "title": "Gestione Musica",
+        "description": "Organizza e riproduci la tua musica, Crea playlist o filtra per generi, artisti o album",
+        "imageUrl": feat_1761582326212_featImg0
+      },
+      {
+        "title": "Film & Serie TV",
+        "description": "Godi i tuoi contenuti video etichettabili e filtrabili per tipo, genere o raccolta",
+        "imageUrl": feat_1761582340878_featImg1
+      },
+      {
+        "title": "Liste IPTV",
+        "description": "Guarda canali da tutto il mondo in chiaro, filtrati per nazione o categoria",
+        "imageUrl": feat_1761582367755_featImg2
+      },
+      {
+        "title": "Libreria Intelligente",
+        "description": "Importa i tuoi eBook e documenti oppure noleggia gratuitamente da OpenLibrary & InternetArchive dalla la sezione dedicata.",
+        "imageUrl": feat_1761582378195_featImg3
+      }
+    ],
+    "repository": "https://github.com/antoniotraversa/TravyPlay"
+  }
 ];
